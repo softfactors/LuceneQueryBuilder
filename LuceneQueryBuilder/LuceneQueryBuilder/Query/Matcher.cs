aad0ea05a4e88ba43753a18628a78fdfd533abc0
davidsulc <david.sulc@softfactors.com>
2020-10-02 14:50:03 +0200

    implement expression matcher

---
+++ LuceneQueryBuilder/LuceneQueryBuilder/Query/Matcher.cs
@@ -0,0 +1,15 @@
+﻿namespace LuceneQueryBuilder.Query
+{
+    public static class Matcher
+    {
+        /// <summary>
+        /// <returns>Returns an expression containing the <c>value</c> constraint on field <c>field</c>.</returns>
+        /// </summary>
+        public static Expression Match(string field, string value) => new Expression(new Constraint(field, value));
+
+        /// <summary>
+        /// <returns>Returns the unmodified expression.</returns>
+        /// </summary>
+        public static Expression Match(Expression e) => e;
+    }
+}
