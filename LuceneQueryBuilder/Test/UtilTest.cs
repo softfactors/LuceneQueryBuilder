57ea88d3877f35bfdac3520cf8ad27c1528e1897
davidsulc <david.sulc@softfactors.com>
2020-10-02 10:58:21 +0200

    introduce Util class

---
+++ LuceneQueryBuilder/Test/UtilTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static LuceneQueryBuilder.Util;
+
+namespace Test
+{
+    [TestClass]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void EscapeTermProperlyEscapes()
+        {
+            foreach (var c in CharsToEscape)
+            {
+                Assert.AreEqual($"\\{c}", EscapeTerm(c));
+            }
+
+            Assert.AreEqual(@"foo \\ bar \+ baz", EscapeTerm(@"foo \ bar + baz"));
+        }
+    }
+}
