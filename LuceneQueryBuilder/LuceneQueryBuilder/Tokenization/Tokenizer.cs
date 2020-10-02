aa564031e7d1932ebe3f2a41444b5b31992f4ec8
davidsulc <david.sulc@softfactors.com>
2020-10-02 12:49:18 +0200

    implement tokenizer

---
+++ LuceneQueryBuilder/LuceneQueryBuilder/Tokenization/Tokenizer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace LuceneQueryBuilder.Tokenization
+{
+    public static class Tokenizer
+    {
+
+        /// <summary>Tokenizes <c>term</c> according to the provided Lucene index analyzer.
+        ///
+        /// Note that not all analyzers are available.
+        ///
+        /// For more information on analyzers, see https://docs.couchdb.org/en/master/ddocs/search.html#analyzers
+        ///
+        /// <example>For example:
+        /// <code>
+        ///    Tokenize("foo/bar0flim flam", Analyzer.Simple)
+        /// </code>
+        /// returns `new[]{ "foo", "bar", "flim", "flam" }`.
+        /// </example>
+        /// </summary>
+        /// <param name="term">String to escape</param>
+        /// <param name="analyzer">Tokenization.Analyzer indicating which tokenization rules to apply</param>
+        /// <returns>an enumerable of string tokens composing the <c>term</c></returns>
+        public static IEnumerable<string> Tokenize(string term, Analyzer analyzer = Analyzer.Whitespace)
+        {
+            switch (analyzer)
+            {
+                case Analyzer.Keyword:
+                    return new[] { term };
+                case Analyzer.Whitespace:
+                    return Tokenize(term, @"\s+");
+                case Analyzer.Simple:
+                    return Tokenize(term, @"[^\p{L}]");
+                default:
+                    throw new Exception($"The {analyzer} analyzer is not supported.");
+            }
+        }
+
+        /// <summary>Tokenizes <c>term</c> according to the <c>boundaryRegex</c> regular expression.
+        ///
+        /// <example>For example:
+        /// <code>
+        ///    Tokenize("foo/bar0flim flam", @"[\d/]")
+        /// </code>
+        /// returns `new[] { "foo", "bar", "flim flam" }`.
+        /// </example>
+        /// </summary>
+        /// <param name="term">String to escape</param>
+        /// <param name="boundaryRegex">String of regular expression to use when tokenizing the <c>term</c></param>
+        /// <returns>an enumerable of string tokens composing the <c>term</c></returns>
+        public static IEnumerable<string> Tokenize(string term, string boundaryRegex) => Regex.Split(term, boundaryRegex);
+    }
+}
---
+++ LuceneQueryBuilder/Test/Tokenization/TokenizerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using LuceneQueryBuilder.Tokenization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static LuceneQueryBuilder.Tokenization.Tokenizer;
+
+namespace Test.Tokenization
+{
+    [TestClass]
+    public class TokenizerTest
+    {
+        private const string Term = "foo/bar0flim flam";
+
+        [TestMethod]
+        public void KeywordTokenizer()
+        {
+            CollectionAssert.AreEqual(new[]{ Term }, Tokenize(Term, Analyzer.Keyword).ToList());
+        }
+
+        [TestMethod]
+        public void SimpleTokenizer()
+        {
+            CollectionAssert.AreEqual(new[]{ "foo", "bar", "flim", "flam" }, Tokenize(Term, Analyzer.Simple).ToList());
+        }
+
+        [TestMethod]
+        public void WhitespaceTokenizer()
+        {
+            // ReSharper disable once RedundantArgumentDefaultValue
+            CollectionAssert.AreEqual(new[] { "foo/bar0flim", "flam" }, Tokenize(Term, Analyzer.Whitespace).ToList());
+        }
+
+        [TestMethod]
+        public void RegexTokenizer()
+        {
+            CollectionAssert.AreEqual(new[] { "foo", "bar", "flim flam" }, Tokenize(Term, @"[\d/]").ToList());
+        }
+    }
+}
