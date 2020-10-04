844d88724d5356584e437e889eb0060d99bb8dde
davidsulc <david.sulc@softfactors.com>
2020-10-04 19:18:39 +0200

    document collection matchers

--- LuceneQueryBuilder/LuceneQueryBuilder/Query/Matcher.cs
+++ LuceneQueryBuilder/LuceneQueryBuilder/Query/Matcher.cs
@@ -16,21 +16,39 @@ namespace LuceneQueryBuilder.Query
         /// </summary>
         public static Expression Match(Expression e) => e;
 
+        /// <summary>
+        /// <returns>Returns an expression matching ALL <c>values</c> (i.e. composed with AND) for field <c>field</c>.</returns>
+        /// </summary>
         public static Expression MatchAll(string field, IEnumerable<string> values) =>
             MatchAll(ToArray(field, values));
 
+        /// <summary>
+        /// <returns>Returns an expression matching ALL <c>expressions</c> (i.e. composed with AND).</returns>
+        /// </summary>
         public static Expression MatchAll(IEnumerable<Expression> expressions) =>
             MatchAll(expressions.ToArray());
 
+        /// <summary>
+        /// <returns>Returns an expression matching ALL <c>expressions</c> (i.e. composed with AND).</returns>
+        /// </summary>
         public static Expression MatchAll(params Expression[] expressions) =>
             Aggregate(expressions, (acc, e) => acc.And(e));
 
+        /// <summary>
+        /// <returns>Returns an expression matching ANY <c>value</c> (i.e. composed with OR) for field <c>field</c>.</returns>
+        /// </summary>
         public static Expression MatchAny(string field, IEnumerable<string> values) =>
             MatchAny(ToArray(field, values));
 
+        /// <summary>
+        /// <returns>Returns an expression matching ANY <c>expression</c> (i.e. composed with OR).</returns>
+        /// </summary>
         public static Expression MatchAny(IEnumerable<Expression> expressions) =>
             MatchAny(expressions.ToArray());
 
+        /// <summary>
+        /// <returns>Returns an expression matching ANY <c>expression</c> (i.e. composed with OR).</returns>
+        /// </summary>
         public static Expression MatchAny(params Expression[] expressions) =>
             Aggregate(expressions, (acc, e) => acc.Or(e));
 
